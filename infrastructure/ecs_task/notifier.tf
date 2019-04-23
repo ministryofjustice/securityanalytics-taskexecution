e8a171587bd06368399cdbe32811064a46a6d4ba
Matthew Amos <35695811+blackmamo@users.noreply.github.com>
2019-04-23 17:33:03 +0100

    Give sns permission to publish to sqs (#17)
    * Give sns permission to publish to sqs

--- infrastructure/ecs_task/notifier.tf
+++ infrastructure/ecs_task/notifier.tf
@@ -1,14 +1,53 @@
 resource "aws_sns_topic" "task_results" {
-  name = "${terraform.workspace}-${var.app_name}-${var.task_name}-results"
+  name         = "${terraform.workspace}-${var.app_name}-${var.task_name}-results"
+  display_name = "SNS topic to distribute ${var.task_name} task results"
 }
 
-data "aws_ssm_parameter" "elastic_injestion_queue" {
+data "aws_ssm_parameter" "elastic_injestion_queue_arn" {
   name = "/${var.app_name}/${var.ssm_source_stage}/analytics/elastic/injest_queue/arn"
 }
 
+data "aws_ssm_parameter" "elastic_injestion_queue_id" {
+  name = "/${var.app_name}/${var.ssm_source_stage}/analytics/elastic/injest_queue/id"
+}
+
+data "aws_iam_policy_document" "notify_topic_policy" {
+  statement {
+    actions = [
+      "sqs:SendMessage",
+    ]
+
+    condition {
+      test     = "ArnEquals"
+      variable = "aws:SourceArn"
+
+      values = [
+        "${aws_sns_topic.task_results.arn}",
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      "${data.aws_ssm_parameter.elastic_injestion_queue_arn.value}",
+    ]
+  }
+}
+
+resource "aws_sqs_queue_policy" "test" {
+  queue_url = "${data.aws_ssm_parameter.elastic_injestion_queue_id.value}"
+  policy    = "${data.aws_iam_policy_document.notify_topic_policy.json}"
+}
+
 resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-  count     = "${var.subscribe_elastic_to_notifier}"
-  topic_arn = "${aws_sns_topic.task_results.arn}"
-  protocol  = "sqs"
-  endpoint  = "${data.aws_ssm_parameter.elastic_injestion_queue.value}"
+  count                = "${var.subscribe_elastic_to_notifier}"
+  topic_arn            = "${aws_sns_topic.task_results.arn}"
+  protocol             = "sqs"
+  endpoint             = "${data.aws_ssm_parameter.elastic_injestion_queue_arn.value}"
+  raw_message_delivery = true
 }
