35457a75c9f7ad71af57689142cd6fdd5c791646
Matthew Amos <35695811+blackmamo@users.noreply.github.com>
2019-04-24 11:34:43 +0100

    Fix ecr auth (#22)

--- infrastructure/ecs_task/update_docker_image.sh
+++ infrastructure/ecs_task/update_docker_image.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-eval "$(aws ecr get-login --profile=$5 --no-include-email --region $4)"
+# Developers will likely have multiple amazon environments setup using profiles, but the build
+# uses environment variables. Support both.
+if [ "$AWS_ACCESS_KEY_ID" == "" ]; then
+    eval "$(aws ecr get-login --profile=$5 --no-include-email --region $4)"
+else
+    eval "$(aws ecr get-login --no-include-email --region $4)"
+fi
 docker build -t $1 $3
 if [ $? -eq 0 ]
 then
