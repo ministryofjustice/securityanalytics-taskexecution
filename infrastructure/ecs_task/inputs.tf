a1f7e870816ebd8d65a8a86c3eab54f2c1a4621b
Matthew Amos <mamos@scottlogic.com>
2019-04-25 09:27:50 +0100

    Move to having per task results buckets

--- infrastructure/ecs_task/inputs.tf
+++ infrastructure/ecs_task/inputs.tf
@@ -42,9 +42,15 @@ variable "vpc_id" {
 
 variable "subscribe_elastic_to_notifier" {
   type = "string"
+  description = "If this task produces elastic compatible output, this can be used to connect the task directly to elastic."
 }
 
 variable "ssm_source_stage" {
   type        = "string"
   description = "When deploying infrastructure for integration tests the source of ssm parameters for e.g. the congnito pool need to come from dev, not from the stage with the same name."
+}
+
+variable "transient_workspace" {
+  type = "string"
+  description = "Used when doing integration tests to make the results buckets created deleteable."
 }
