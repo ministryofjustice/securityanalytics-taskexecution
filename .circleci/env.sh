e71f021c400f68e1297b6f1b7818d0640fd3c130
Phil Rogers <progers@scottlogic.com>
2019-05-01 08:59:21 +0100

    reduced env.sh to what we need

--- .circleci/env.sh
+++ .circleci/env.sh
@@ -1,14 +1,4 @@
 #!/usr/bin/sh
 
-# . ./.venv/bin/activate
-# export AWS_REGION=eu-west-2
-# export PYTHONPATH=`pwd`
-# export PYTHONPATH=$PYTHONPATH:`pwd`/shared_code/python
-# export USERNAME=builder
-
-# # Get short version of git hash for temporary deployment
-# export SOURCE_VERSION=`git rev-parse HEAD`
-# export DEPLOY_STAGE=$(expr substr $SOURCE_VERSION 1 8)
-
-# # TODO change this when we have more stages than just dev
+# TODO change this when we have more stages than just dev
 export SSM_SOURCE_STAGE=dev
