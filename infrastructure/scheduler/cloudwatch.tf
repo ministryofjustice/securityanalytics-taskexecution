6963d96a0eca6b3aadeffdc5f02f611e2d6362c2
Phil Rogers <progers@scottlogic.com>
2019-05-01 21:31:18 +0100

    fixed the maths around how tasks are added to rules, and made code easier to adapt later (see PR notes for more...)

--- infrastructure/scheduler/cloudwatch.tf
+++ infrastructure/scheduler/cloudwatch.tf
@@ -1,22 +1,31 @@
+locals {
+  max_rules     = 80
+  planned_count = "${min(local.max_rules,length(var.scan_hosts))}"
+}
+
 resource "random_integer" "time_delay" {
   min   = 0
   max   = 59
   seed  = "${var.scan_hosts[count.index]}"
-  count = "${(length(var.scan_hosts)>80)?80:length(var.scan_hosts)}"
+  count = "${local.planned_count}"
 }
 
 resource "aws_cloudwatch_event_target" "task_scheduler" {
-  count = "${(length(var.scan_hosts)>80)?80:length(var.scan_hosts)}"
+  count = "${local.planned_count}"
   rule  = "${aws_cloudwatch_event_rule.task_scheduler_sqs_rule.*.name[count.index]}"
   arn   = "${var.queue_arn}"
-  input = "{\"CloudWatchEventHosts\":[\"${join("\",\"",slice(var.scan_hosts, floor(count.index*(length(var.scan_hosts)/80)), ceil((count.index+1)*(length(var.scan_hosts)/80)) ) )}\"]}"
+  input = "{\"CloudWatchEventHosts\":[\"${join("\",\"",slice(var.scan_hosts, floor(count.index*(length(var.scan_hosts)/local.planned_count)), ceil((count.index+1)*(length(var.scan_hosts)/local.planned_count)) ) )}\"]}"
 }
 
 resource "aws_cloudwatch_event_rule" "task_scheduler_sqs_rule" {
-  count       = "${(length(var.scan_hosts)>80)?80:length(var.scan_hosts)}"
+  count       = "${local.planned_count}"
   name        = "nmap_task_${count.index}"
   description = "Add hosts to the queue to run nmap"
 
   # build cron expression manually based on running each hour
   schedule_expression = "cron(${random_integer.time_delay.*.result[count.index]} * * * ? *)"
+}
+
+output "planned_count" {
+  value = "${local.planned_count}"
 }
