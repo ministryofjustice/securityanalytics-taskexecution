d2685ae8cd8669e85c884134859d1b0fa5379260
Phil Rogers <progers@scottlogic.com>
2019-05-02 10:32:10 +0100

    floor/ceil not needed, added workspace to seed

--- infrastructure/scheduler/cloudwatch.tf
+++ infrastructure/scheduler/cloudwatch.tf
@@ -6,7 +6,7 @@ locals {
 resource "random_integer" "time_delay" {
   min   = 0
   max   = 59
-  seed  = "${var.scan_hosts[count.index]}"
+  seed  = "${terraform.workspace}_${var.scan_hosts[count.index]}"
   count = "${local.planned_count}"
 }
 
@@ -14,7 +14,7 @@ resource "aws_cloudwatch_event_target" "task_scheduler" {
   count = "${local.planned_count}"
   rule  = "${aws_cloudwatch_event_rule.task_scheduler_sqs_rule.*.name[count.index]}"
   arn   = "${var.queue_arn}"
-  input = "{\"CloudWatchEventHosts\":[\"${join("\",\"",slice(var.scan_hosts, floor(count.index*(length(var.scan_hosts)/local.planned_count)), ceil((count.index+1)*(length(var.scan_hosts)/local.planned_count)) ) )}\"]}"
+  input = "{\"CloudWatchEventHosts\":[\"${join("\",\"",slice(var.scan_hosts, (count.index*length(var.scan_hosts))/local.planned_count, ((count.index+1)*length(var.scan_hosts))/local.planned_count) )}\"]}"
 }
 
 resource "aws_cloudwatch_event_rule" "task_scheduler_sqs_rule" {
@@ -24,8 +24,4 @@ resource "aws_cloudwatch_event_rule" "task_scheduler_sqs_rule" {
 
   # build cron expression manually based on running each hour
   schedule_expression = "cron(${random_integer.time_delay.*.result[count.index]} * * * ? *)"
-}
-
-output "planned_count" {
-  value = "${local.planned_count}"
 }
