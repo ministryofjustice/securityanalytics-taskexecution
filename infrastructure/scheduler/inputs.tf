eea5388d21ffb1775fd798d7c25cd6be8989d97d
Phil Rogers <progers@scottlogic.com>
2019-05-03 14:33:00 +0100

    ssm_source_stage not used - integration tests then broke the build!

--- infrastructure/scheduler/inputs.tf
+++ infrastructure/scheduler/inputs.tf
@@ -18,6 +18,11 @@ variable "scan_hosts" {
   type = "list"
 }
 
+variable "ssm_source_stage" {
+  type        = "string"
+  description = "When deploying infrastructure for integration tests the source of ssm parameters for e.g. the congnito pool need to come from dev, not from the stage with the same name."
+}
+
 variable "transient_workspace" {
   type        = "string"
   description = "Used when doing integration tests to make the results buckets created deleteable."
