609ffaab16c03ffde506e705b4ddb1edd3ab5bea
Phil Rogers <progers@scottlogic.com>
2019-05-15 11:28:34 +0100

    rename, sleep comment

--- setup.sh
+++ setup.sh
@@ -10,5 +10,5 @@ cd infrastructure
 terraform init -backend-config "bucket=$1-terraform-state"
 terraform workspace new $2 || terraform workspace select $2
 terraform apply -auto-approve -input=true
-
+# pause in case the user is watching output
 sleep 5
