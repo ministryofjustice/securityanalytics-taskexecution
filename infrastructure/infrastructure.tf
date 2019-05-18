49c0cab59d8bfd7a5e54a9bf4638711c5eb010d3
Phil Rogers <progers@scottlogic.com>
2019-05-18 20:45:48 +0100

    terraform fmt

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -38,7 +38,8 @@ variable "scan_hosts" {
 }
 
 provider "aws" {
-  region              = "${var.aws_region}"
+  region = "${var.aws_region}"
+
   # profile set in env variables to support MFA
   # profile = "${var.app_name}"
   allowed_account_ids = ["${var.account_id}"]
