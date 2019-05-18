ce988bcdc3aa29c888ce3209e8d9ef6e6d6a2064
Phil Rogers <progers@scottlogic.com>
2019-05-18 16:45:46 +0100

    macos script compatibility

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@ fi
 cd infrastructure
 terraform init -backend-config "bucket=$1-terraform-state"
 terraform workspace new $2 || terraform workspace select $2
-terraform apply -auto-approve -input=true
+terraform apply -auto-approve -input=true -var app_name=$1
 wait
 # pause in case the user is watching output
 sleep 5
