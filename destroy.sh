01d10368f77cd25a0bc2a5737548e9f439a3e537
Phil Rogers <47106782+progers-SL@users.noreply.github.com>
2019-05-20 11:16:17 +0100

    Feature/sa 97 build scripts (#43)
    * macos script compatibility

--- destroy.sh
+++ destroy.sh
@@ -2,14 +2,15 @@
 
 if [ $# -ne 2 ]
 then
-    echo "Syntax: setup.sh <app_name> <tf_workspace>"
+    echo "Syntax: destroy.sh <app_name> <tf_workspace>"
     sleep 30
     exit
 fi
+
 cd infrastructure
 terraform init -backend-config "bucket=$1-terraform-state"
 terraform workspace new $2 || terraform workspace select $2
-terraform apply -auto-approve -input=true -var app_name=$1
+terraform destroy -auto-approve -input=true -var app_name=$1
 wait
 # pause in case the user is watching output
 sleep 5
--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -38,8 +38,9 @@ variable "scan_hosts" {
 }
 
 provider "aws" {
-  region              = "${var.aws_region}"
-  profile             = "${var.app_name}"
+  region = "${var.aws_region}"
+
+  # N.B. To support all authentication use cases, we expect the local environment variables to provide auth details.
   allowed_account_ids = ["${var.account_id}"]
 }
 
