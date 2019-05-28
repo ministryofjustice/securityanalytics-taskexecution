2347fd259296bdd4d9a77be6d325e163fa55b545
Matthew Amos <mamos@scottlogic.com>
2019-05-28 21:30:28 +0100

    WIP

--- dns_ingestor/ingestor.py
+++ dns_ingestor/ingestor.py
@@ -54,13 +54,13 @@ class DnsIngestor:
     async def _process_hosted_zone(self, zone):
         kwargs = {"MaxItems": "1000"}
         finished_listing_resources = False
-        ingested = 0
+        db_writes = []
         while not finished_listing_resources:
             details_resp = await self.route53_client.list_resource_record_sets(HostedZoneId=zone["Id"], **kwargs)
 
-            await gather(*[
+            db_writes += [
                 self._dispatch_record(record) for record in details_resp["ResourceRecordSets"]
-            ])
+            ]
 
             # this block updates the paging details for the next list_resource_record_sets call
             finished_listing_resources = not bool(details_resp["IsTruncated"])
@@ -73,24 +73,24 @@ class DnsIngestor:
                     del kwargs["StartRecordIdentifier"]
 
             # there is a 5 requests per second limit on aws for route53 calls
-            # conservative here to ensure we finish the scan
             time.sleep(1.0/5.0)
 
-        print(f"Ingested zone: {zone['Name']}, {ingested} records")
-        return ingested
+        print(f"Ingested zone: {zone['Name']}, {len(db_writes)} records")
+        for writes in db_writes:
+            await writes
+        return len(db_writes)
 
     async def _dispatch_record(self, record):
         record_type = record["Type"]
         if record_type in self._record_type_handler.keys():
             return await self._record_type_handler[record_type](record)
         else:
-            return self._process_other_record(record)
+            return await self._process_other_record(record)
 
     async def _scan_hosts(self, hosts_to_scan):
-        await gather(*[
+        return gather(*[
             self.out_writer.write(host_to_scan) for host_to_scan in hosts_to_scan
         ])
-        return len(hosts_to_scan)
 
     async def _process_a_record(self, record):
         if "AliasTarget" in record:
@@ -98,47 +98,48 @@ class DnsIngestor:
         else:
             print(f"Ingested {record['Type']} record: {record['Name']}")
             records = record["ResourceRecords"]
-            return await self._scan_hosts([
+            result = await self._scan_hosts([
                 HostToScan(resource["Value"], record["Name"]) for resource in records
             ])
+            return result
 
     async def _process_alias_record(self, record):
         print(f"Ingested ALIAS record: {record['Name']}")
         target = record["AliasTarget"]
         alias_zone = target["HostedZoneId"]
-        record_count = 0
+        writes = []
         if alias_zone in self.to_ingest.keys() or alias_zone in self.ingested:
             print(f"Ignoring alias to zone already being scanned {alias_zone} referred to by {record['Name']}")
         else:
             redirect_to = target["DNSName"]
             try:
-                record_count += await self._scan_hosts([
+                writes += await self._scan_hosts([
                     HostToScan(ip, record["Name"]) for ip in await ns_lookup(redirect_to)
                 ])
                 print(f"Resolved ip addresses of {redirect_to} an ALIAS of {record['Name']}")
             except (RuntimeError, gaierror):
                 print(f"Failed to resolve {redirect_to} an ALIAS of {record['Name']}")
-        return record_count
+        return writes
 
     async def _process_cname_record(self, record):
         print(f"Ingested {record['Type']} record: {record['Name']}")
         records = record["ResourceRecords"]
-        record_count = 0
+        writes = []
         for resource in records:
             # strip the dot
             redirect_to = resource["Value"]
             try:
 
-                record_count += await self._scan_hosts([
+                writes += await self._scan_hosts([
                     HostToScan(ip, record["Name"]) for ip in await ns_lookup(redirect_to)
                 ])
                 print(f"Resolved ip addresses of {redirect_to} an {record['Type']} of {record['Name']}")
             except (RuntimeError, gaierror):
                 print(f"Failed to resolve {redirect_to} a {record['Type']} of {record['Name']}")
 
-        return record_count
+        return writes
 
-    def _process_other_record(self, record):
+    async def _process_other_record(self, record):
         if self.log_unhandled:
             print(f"Unhandled record {record['Type']}: {record}")
-        return 0
+        return []
