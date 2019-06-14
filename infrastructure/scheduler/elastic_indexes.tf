23afe19602397d07a6402464bb186cc5f88937da
Matthew Amos <mamos@scottlogic.com>
2019-06-14 10:23:10 +0100

    oops on relative path not git path

--- infrastructure/scheduler/elastic_indexes.tf
+++ infrastructure/scheduler/elastic_indexes.tf
@@ -1,13 +1,13 @@
 module "scan_plan_index" {
    # two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories
- # source = "github.com/ministryofjustice/securityanalytics-analyticsplatform//infrastructure/elastic_index"
+ source = "github.com/ministryofjustice/securityanalytics-analyticsplatform//infrastructure/elastic_index"
 
  # It is sometimes useful for the developers of the project to use a local version of the task
  # execution project. This enables them to develop the task execution project and the nmap scanner
  # (or other future tasks), at the same time, without requiring the task execution changes to be
  # pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
  # devs will have to comment in/out this line as and when they need
- source = "../../../securityanalytics-analyticsplatform/infrastructure/elastic_index"
+ # source = "../../../securityanalytics-analyticsplatform/infrastructure/elastic_index"
   app_name = var.app_name
   aws_region       = var.aws_region
   ssm_source_stage = var.ssm_source_stage
