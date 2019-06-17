da4bb4a4686b265ca6b497266d55abb646306eff
Phil Rogers <47106782+progers-SL@users.noreply.github.com>
2019-06-17 09:01:41 +0100

    removed todos (#61)

--- infrastructure/infrastructure.tf
+++ infrastructure/infrastructure.tf
@@ -70,25 +70,11 @@ locals {
 }
 
 module "ecs_cluster" {
-  # TF-UPGRADE-TODO: In Terraform v0.11 and earlier, it was possible to
-  # reference a relative module source without a preceding ./, but it is no
-  # longer supported in Terraform v0.12.
-  #
-  # If the below module source is indeed a relative local path, add ./ to the
-  # start of the source string. If that is not the case, then leave it as-is
-  # and remove this TODO comment.
   source   = "./ecs_cluster"
   app_name = var.app_name
 }
 
 module "scheduler" {
-  # TF-UPGRADE-TODO: In Terraform v0.11 and earlier, it was possible to
-  # reference a relative module source without a preceding ./, but it is no
-  # longer supported in Terraform v0.12.
-  #
-  # If the below module source is indeed a relative local path, add ./ to the
-  # start of the source string. If that is not the case, then leave it as-is
-  # and remove this TODO comment.
   source           = "./scheduler"
   aws_region       = var.aws_region
   account_id       = var.account_id
