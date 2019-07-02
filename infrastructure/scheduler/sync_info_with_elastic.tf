6daeb8b569bf8757d9285dedcbf61da6e23e17b2
Matthew Amos <mamos@scottlogic.com>
2019-07-02 16:02:25 +0100

    fix bad merge

--- infrastructure/scheduler/sync_info_with_elastic.tf
+++ infrastructure/scheduler/sync_info_with_elastic.tf
@@ -37,9 +37,6 @@ module "sync_resolved_hosts" {
   account_id       = var.account_id
   app_name         = var.app_name
   aws_region       = var.aws_region
-  account_id = var.account_id
-  app_name = var.app_name
-  aws_region = var.aws_region
   ssm_source_stage = var.ssm_source_stage
   use_xray         = var.use_xray
 
