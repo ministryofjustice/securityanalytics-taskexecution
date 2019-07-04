a994da28ed34e1377568433627592768e97074af
Matthew Amos <mamos@scottlogic.com>
2019-07-04 12:57:18 +0100

    WIP

---
+++ infrastructure/ecs_task/ssm_exports.tf
@@ -0,0 +1,25 @@
+resource "aws_ssm_parameter" "security_group" {
+  name        = "/${var.app_name}/${terraform.workspace}/tasks/${var.task_name}/security_group/id"
+  description = "The task's security group"
+  type        = "String"
+  value       = aws_security_group.task_group.id
+  overwrite   = "true"
+
+  tags = {
+    app_name  = var.app_name
+    workspace = terraform.workspace
+  }
+}
+
+resource "aws_ssm_parameter" "image_id" {
+  name        = "/${var.app_name}/${terraform.workspace}/tasks/${var.task_name}/image/id"
+  description = "The task images id"
+  type        = "String"
+  value       = "${aws_ecs_task_definition.task.family}:${aws_ecs_task_definition.task.revision}"
+  overwrite   = "true"
+
+  tags = {
+    app_name  = var.app_name
+    workspace = terraform.workspace
+  }
+}
