ac48ed20f63bfe8b3d71cca7f1cfa9a15ea90763
Phil Rogers <47106782+progers-SL@users.noreply.github.com>
2019-07-12 18:03:07 +0100

    new dashboards for actionable items (#75)
    * new dashboards for actionable items

--- infrastructure/scheduler/elastic_dashboards.tf
+++ infrastructure/scheduler/elastic_dashboards.tf
@@ -1,15 +1,15 @@
 
 
 module "dns_dashboard" {
-  // two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories
+  # two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories
   source = "github.com/ministryofjustice/securityanalytics-analyticsplatform//infrastructure/kibana_saved_object"
 
-  // It is sometimes useful for the developers of the project to use a local version of the task
-  // execution project. This enables them to develop the task execution project and the nmap scanner
-  // (or other future tasks), at the same time, without requiring the task execution changes to be
-  // pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
-  // devs will have to comment in/out this line as and when they need
-  //  source = "../../../securityanalytics-analyticsplatform/infrastructure/kibana_saved_object"
+  # It is sometimes useful for the developers of the project to use a local version of the task
+  # execution project. This enables them to develop the task execution project and the nmap scanner
+  # (or other future tasks), at the same time, without requiring the task execution changes to be
+  # pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
+  # devs will have to comment in/out this line as and when they need
+  #  source = "../../../securityanalytics-analyticsplatform/infrastructure/kibana_saved_object"
   app_name = var.app_name
 
   aws_region       = var.aws_region
@@ -31,5 +31,32 @@ module "dns_dashboard" {
 
   object_type  = "dashboard"
   object_title = "DNS Ingestion and Dead Letters"
+  es_domain    = data.aws_ssm_parameter.es_domain.value
+}
+
+module "ai_dns_dlq_dashboard" {
+  # two slashes are intentional: https://www.terraform.io/docs/modules/sources.html#modules-in-package-sub-directories
+  source = "github.com/ministryofjustice/securityanalytics-analyticsplatform//infrastructure/kibana_saved_object"
+
+  # It is sometimes useful for the developers of the project to use a local version of the task
+  # execution project. This enables them to develop the task execution project and the nmap scanner
+  # (or other future tasks), at the same time, without requiring the task execution changes to be
+  # pushed to master. Unfortunately you can not interpolate variables to generate source locations, so
+  # devs will have to comment in/out this line as and when they need
+  #  source = "../../../securityanalytics-analyticsplatform/infrastructure/kibana_saved_object"
+  app_name = var.app_name
+
+  aws_region       = var.aws_region
+  ssm_source_stage = var.ssm_source_stage
+  object_template  = "${path.module}/dashboards/ai_dns_dlq.dash.json"
+
+  object_substitutions = {
+    dead_letters      = data.aws_ssm_parameter.dead_letter_index_pattern.value
+    changed_addresses = module.resolved_address_changes_vis.object_id
+    changed_hosts     = module.resolved_host_changes_vis.object_id
+  }
+
+  object_type  = "dashboard"
+  object_title = "Actionable items: DNS changes and DLQs"
   es_domain    = data.aws_ssm_parameter.es_domain.value
 }
